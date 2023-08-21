c3403f47ed9a8e9a3e7c63a6c583c6c5903fa3bc
filippo199600 <pippotartaglia@gmail.com>
2023-08-21 16:40:55 +0200

    js function createCell

--- js/main.js
+++ js/main.js
@@ -11,19 +11,25 @@ buttonGrid.addEventListener("click", function () {
 function getGenerateGrid() {
   // variabile cella inserita in un ciclo for
   for (let i = 1; i <= 100; i++) {
-    // creo la mia cella
-    const cell = document.createElement("div");
-    cell.classList.add("ten");
-    cell.innerHTML = i;
-
-    // stabilisco un evento al click della cella
-    cell.addEventListener("click", function () {
-      // aggiungo la classe colore
-      cell.classList.add("color-click");
-      console.log(i);
-    });
-
+    const cell = createCell(i);
     // stampo la cella
     gridElement.append(cell);
   }
+}
+
+// funzione che genera celle
+function createCell(i) {
+  // creo la mia cella
+  const cell = document.createElement("div");
+  cell.classList.add("ten");
+  cell.innerHTML = i;
+
+  // stabilisco un evento al click della cella
+  cell.addEventListener("click", function () {
+    // aggiungo la classe colore
+    cell.classList.add("color-click");
+    console.log(i);
+  });
+
+  return cell;
 }
