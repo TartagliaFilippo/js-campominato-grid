d200a5074a6c2f397f5860b08927a358b9b93b9a
filippo199600 <pippotartaglia@gmail.com>
2023-08-21 16:33:55 +0200

    js function and add style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-// collego il bottone a un evento
-const generateGrid = document.getElementById("generate-grid");
+// collego il contenitore della griglia
+const gridElement = document.getElementById("grid");
 
-generateGrid.addEventListener("click", function () {});
+// collego il bottone a un evento
+const buttonGrid = document.getElementById("generate-grid");
+
+buttonGrid.addEventListener("click", function () {
+  getGenerateGrid();
+});
+
+function getGenerateGrid() {
+  // variabile cella inserita in un ciclo for
+  for (let i = 1; i <= 100; i++) {
+    // creo la mia cella
+    const cell = document.createElement("div");
+    cell.classList.add("ten");
+    cell.innerHTML = i;
+
+    // stabilisco un evento al click della cella
+    cell.addEventListener("click", function () {
+      // aggiungo la classe colore
+      cell.classList.add("color-click");
+      console.log(i);
+    });
+
+    // stampo la cella
+    gridElement.append(cell);
+  }
+}
